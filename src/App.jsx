a6d7919945f734cbede3589066954e42c66ba086
Lokendra Dewangan <lokendra.intern@globussoft.in>
2023-06-27 17:14:05 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Analytics from './components/Analytics';
+import Card from './components/Card';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import NewsLetter from './components/NewsLetter';
+
+function App() {
+  return (
+  <div>
+    <Navbar />
+    <Hero />
+    <Analytics />
+    <NewsLetter />
+    <Card />
+    <Footer />
+  </div>
+  );
+}
+
+export default App
